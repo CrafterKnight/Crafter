31387bd11d6d3228b2f7a9c4754dde163487bb8d
Alexander01998 <contact.wurstimperium@gmail.com>
2019-08-23 04:48:08 +0200

    Fix XRayHack's default ores

--- src/main/java/net/wurstclient/hacks/XRayHack.java
+++ src/main/java/net/wurstclient/hacks/XRayHack.java
@@ -28,7 +28,25 @@ public final class XRayHack extends Hack implements UpdateListener,
 	SetOpaqueCubeListener, GetAmbientOcclusionLightLevelListener,
 	ShouldDrawSideListener, TesselateBlockListener, RenderBlockEntityListener
 {
-	private final BlockListSetting ores = new BlockListSetting("Ores");
+	private final BlockListSetting ores = new BlockListSetting("Ores", "",
+		"minecraft:anvil", "minecraft:beacon", "minecraft:bone_block",
+		"minecraft:bookshelf", "minecraft:brewing_stand",
+		"minecraft:chain_command_block", "minecraft:chest", "minecraft:clay",
+		"minecraft:coal_block", "minecraft:coal_ore", "minecraft:command_block",
+		"minecraft:crafting_table", "minecraft:diamond_block",
+		"minecraft:diamond_ore", "minecraft:dispenser", "minecraft:dropper",
+		"minecraft:emerald_block", "minecraft:emerald_ore",
+		"minecraft:enchanting_table", "minecraft:end_portal",
+		"minecraft:end_portal_frame", "minecraft:ender_chest",
+		"minecraft:furnace", "minecraft:glowstone", "minecraft:gold_block",
+		"minecraft:gold_ore", "minecraft:hopper", "minecraft:iron_block",
+		"minecraft:iron_ore", "minecraft:ladder", "minecraft:lapis_block",
+		"minecraft:lapis_ore", "minecraft:lava", "minecraft:mossy_cobblestone",
+		"minecraft:nether_portal", "minecraft:nether_quartz_ore",
+		"minecraft:redstone_block", "minecraft:redstone_ore",
+		"minecraft:repeating_command_block", "minecraft:spawner",
+		"minecraft:tnt", "minecraft:torch", "minecraft:trapped_chest",
+		"minecraft:water");
 	
 	private ArrayList<String> oreNames;
 	
