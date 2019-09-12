73566792e0714adfd508bc773b39f0d69f51e3c8
Alexander01998 <contact.wurstimperium@gmail.com>
2019-09-12 04:41:36 +0200

    Update to 19w37a

--- src/main/java/net/wurstclient/WurstClient.java
+++ src/main/java/net/wurstclient/WurstClient.java
@@ -40,7 +40,7 @@ public enum WurstClient
 	public static final IMinecraftClient IMC = (IMinecraftClient)MC;
 	
 	public static final String VERSION = "7.0pre7";
-	public static final String MC_VERSION = "1.14.4";
+	public static final String MC_VERSION = "19w37a";
 	
 	private WurstAnalytics analytics;
 	private EventManager eventManager;
